7c5f18bd5e8f5200b93e0cf0d914980ddb2d0f3c
moloch00464-bit <moloch00464@googlemail.com>
2026-03-04 10:41:04 +0100

    BACKUP vor tracking_diagnose.py Script

--- core/hardware/camera.py
+++ core/hardware/camera.py
@@ -718,7 +718,7 @@ class SonoffCameraController:
 
         self._set_tracking_state(TrackingState.TRACKING)
 
-        pan_delta = error_x * self.FOV_HORIZONTAL * self.TRACKING_GAIN_PAN
+        pan_delta = -error_x * self.FOV_HORIZONTAL * self.TRACKING_GAIN_PAN
         tilt_delta = -error_y * self.FOV_VERTICAL * self.TRACKING_GAIN_TILT
 
         pan_delta = max(-self.MAX_STEP_PAN, min(self.MAX_STEP_PAN, pan_delta))
